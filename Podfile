da4614f9aa93d3ef764fe64315bd88d592861917
Michiel Everts <michieleverts-18@hotmail.nl>
2017-10-26 16:02:52 +0200

    alles van de eerste maand bootcamp

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'shoppingList' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for shoppingList
+pod 'Firebase'
+pod 'Firebase/Database'
+pod 'Kingfisher', '~> 4.0'
+
+  target 'shoppingListTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'shoppingListUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ arApp/arBox/arBox/ViewController.swift
@@ -0,0 +1,133 @@
+//
+//  ViewController.swift
+//  arBox
+//
+//  Created by Michiel Everts on 25-10-17.
+//  Copyright © 2017 Michiel Everts. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    @IBAction func addCube(_ sender: Any) {
+        let cubeNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
+        let cc = getCameraCoordinates(sceneView: sceneView)
+        cubeNode.position = SCNVector3(cc.x, cc.y, cc.z)
+        sceneView.scene.rootNode.addChildNode(cubeNode)
+    }
+    @IBAction func addCup(_ sender: Any) {
+        let cupNode = SCNNode()
+        
+        let cc = getCameraCoordinates(sceneView: sceneView)
+        cupNode.position = SCNVector3(cc.x, cc.y, cc.z)
+        
+        guard let virtualObjectScene = SCNScene(named: "cup.scn", inDirectory: "Models.scnassets/cup") else {
+            return
+        }
+        
+        let wrapperNode = SCNNode()
+        for child in virtualObjectScene.rootNode.childNodes {
+            child.geometry?.firstMaterial?.lightingModel = .physicallyBased
+            wrapperNode.addChildNode(child)
+        }
+        cupNode.addChildNode(wrapperNode)
+        
+        sceneView.scene.rootNode.addChildNode(cupNode)
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        sceneView.session.run(configuration)
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+    }
+    
+    func randomFloat(min: Float, max: Float) -> Float {
+        return (Float(arc4random()) / 0xFFFFFFFF) * (max - min) + min
+    }
+    
+    struct myCameraCoordinates {
+        var x = Float()
+        var y = Float()
+        var z = Float()
+    }
+    
+    func getCameraCoordinates(sceneView: ARSCNView) -> myCameraCoordinates {
+        let cameraTransform = sceneView.session.currentFrame?.camera.transform
+        let cameraCoordinates = MDLTransform(matrix: cameraTransform!)
+        
+        var cc = myCameraCoordinates()
+        cc.x = cameraCoordinates.translation.x
+        cc.y = cameraCoordinates.translation.y
+        cc.z = cameraCoordinates.translation.z
+        
+        return cc
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
